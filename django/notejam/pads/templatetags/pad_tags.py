d58d4406aba263828de0f9422e6ca8e24b3c0f8a
Igoni <joshua.igoni@enocean.com>
2025-08-03 06:26:37 +0200

    updated pads to see lists of pads in UI

--- django/notejam/pads/templatetags/pad_tags.py
+++ django/notejam/pads/templatetags/pad_tags.py
@@ -1,5 +1,5 @@
+# pads/templatetags/pad_tags.py
 from django import template
-
 from pads.models import Pad
 
 register = template.Library()
@@ -10,13 +10,13 @@ def do_get_pads(parser, token):
         tag_name, as_, var_name = token.split_contents()
     except ValueError:
         raise template.TemplateSyntaxError(
-            "%r tag requires a single argument - var name"
-            % token.contents.split()[0]
+            "%r tag requires the syntax:  {% %s as <varname> %}"
+            % (token.contents.split()[0], token.contents.split()[0])
         )
-    if as_ != 'as':
-        raise template.TemplateSyntaxError(
-            "Format is: %r as VARNAME" % tag_name
-        )
+
+    if as_ != "as":
+        raise template.TemplateSyntaxError("Format is: %s as VARNAME" % tag_name)
+
     return GetPadsNode(var_name)
 
 
@@ -25,18 +25,15 @@ class GetPadsNode(template.Node):
         self.var_name = var_name
 
     def render(self, context):
+     
         request = context.get("request")
-        user = getattr(request, "user", None)
-
-        if user is None or not hasattr(user, "is_authenticated"):
-            user = context.get("user")
+        user    = getattr(request, "user", None)
 
         if user and user.is_authenticated:
-            pads_qs = Pad.objects.filter(user=user)
+            context[self.var_name] = Pad.objects.filter(user=user)
         else:
-            pads_qs = Pad.objects.none()
-
-        context[self.var_name] = pads_qs
+            context[self.var_name] = Pad.objects.none()
         return ""
 
-register.tag('get_pads', do_get_pads)
+
+register.tag("get_pads", do_get_pads)
